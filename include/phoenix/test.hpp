485faa8776fc349ff5d78099ba7eedd17299751b
SteelPh0enix <wojciech_olech@hotmail.com>
2018-03-17 18:10:15 +0100

    This intend was hurting my eyes

--- include/phoenix/test.hpp
+++ include/phoenix/test.hpp
@@ -5,18 +5,17 @@
 #include <iostream>
 
 namespace phoenix {
+  class test_exception : public std::exception {
+   public:
+    test_exception(const std::string& message) : _message{message} {}
+    // what a monster
+    virtual const char* what() const noexcept override {
+      return _message.c_str();
+    }
 
-class test_exception : public std::exception {
- public:
-  test_exception(const std::string& message) : _message{message} {}
-  // what a monster
-  virtual const char* what() const noexcept override {
-    return _message.c_str();
-  }
-
- private:
-  std::string _message;
-};
+   private:
+    std::string _message;
+  };
 
   template<typename F, typename OutputStream = std::ostream>
   void run_test(F test_function, const std::string& test_name, OutputStream& output = std::cout) {
@@ -31,126 +30,126 @@ class test_exception : public std::exception {
     output << test_name << " test passed!" << std::endl;
   }
 
-class test {
- public:
-  template <typename T1, typename T2>
-  static void eq(const T1& first, const T2& second,
-                 const std::string& message = std::string{}) {
-    if (first != second) {
-      std::stringstream exmsg_stream;
-      exmsg_stream << "<test::eq failed!> " << message << " [first: " << first
-                   << "] [second: " << second << "]";
-      throw test_exception(exmsg_stream.str());
-    }
-  }
-
-  template <typename T1, typename T2>
-  static void neq(const T1& first, const T2& second,
-                  const std::string& message = std::string{}) {
-    if (first == second) {
-      std::stringstream exmsg_stream;
-      exmsg_stream << "<test::neq failed!> " << message << " [first: " << first
-                   << "] [second: " << second << "]";
-      throw test_exception(exmsg_stream.str());
-    }
-  }
-
-  template <typename T1, typename T2>
-  static void gt(const T1& first, const T2& second,
-                 const std::string& message = std::string{}) {
-    if (first <= second) {
-      std::stringstream exmsg_stream;
-      exmsg_stream << "<test::gt failed!> " << message << " [first: " << first
-                   << "] [second: " << second << "]";
-      throw test_exception(exmsg_stream.str());
-    }
-  }
-
-  template <typename T1, typename T2>
-  static void lt(const T1& first, const T2& second,
-                 const std::string& message = std::string{}) {
-    if (first >= second) {
-      std::stringstream exmsg_stream;
-      exmsg_stream << "<test::lt failed!> " << message << " [first: " << first
-                   << "] [second: " << second << "]";
-      throw test_exception(exmsg_stream.str());
-    }
-  }
-
-  template <typename T1, typename T2>
-  static void geq(const T1& first, const T2& second,
-                  const std::string& message = std::string{}) {
-    if (first < second) {
-      std::stringstream exmsg_stream;
-      exmsg_stream << "<test::geq failed!> " << message << " [first: " << first
-                   << "] [second: " << second << "]";
-      throw test_exception(exmsg_stream.str());
-    }
-  }
-
-  template <typename T1, typename T2>
-  static void leq(const T1& first, const T2& second,
-                  const std::string& message = std::string{}) {
-    if (first > second) {
-      std::stringstream exmsg_stream;
-      exmsg_stream << "<test::leq failed!> " << message << " [first: " << first
-                   << "] [second: " << second << "]";
-      throw test_exception(exmsg_stream.str());
-    }
-  }
-
-  template <typename T1, typename T2>
-  static void container_equal(const T1& first, const T2& second,
-                              const std::string& message = std::string{}) {
-    if (first.size() != second.size()) {
-      std::stringstream exmsg_stream;
-      exmsg_stream << "<test::container_equal failed: "
-                   << (first.size() > second.size() ? "first" : "second")
-                   << " container is bigger> " << message;
-      throw test_exception(exmsg_stream.str());
-    }
-    auto first_it = first.cbegin();
-    auto second_it = second.cbegin();
-    std::size_t i = 0;
-    // Since containers have same lengths...
-    while (first_it != first.cend()) {
-      if (*first_it != *second_it) {
+  class test {
+   public:
+    template <typename T1, typename T2>
+    static void eq(const T1& first, const T2& second,
+                   const std::string& message = std::string{}) {
+      if (first != second) {
         std::stringstream exmsg_stream;
-        exmsg_stream << "<test::container_equal failed: " << *first_it
-                     << " != " << *second_it << " at index #" << i << "> "
-                     << message;
+        exmsg_stream << "<test::eq failed!> " << message << " [first: " << first
+                     << "] [second: " << second << "]";
         throw test_exception(exmsg_stream.str());
       }
-      first_it++;
-      second_it++;
-      i++;
     }
-  }
 
-  template <typename T1, typename T2>
-  static void container_not_equal(const T1& first, const T2& second,
-                                  const std::string& message = std::string{}) {
-    if (first.size() != second.size()) return;
-
-    auto first_it = first.cbegin();
-    auto second_it = second.cbegin();
-    bool eq_flag = true;
-    // Since containers have same lengths...
-    while (first_it != first.cend()) {
-      if (*first_it != *second_it) {
-        eq_flag = false;
-        break;
+    template <typename T1, typename T2>
+    static void neq(const T1& first, const T2& second,
+                    const std::string& message = std::string{}) {
+      if (first == second) {
+        std::stringstream exmsg_stream;
+        exmsg_stream << "<test::neq failed!> " << message << " [first: " << first
+                     << "] [second: " << second << "]";
+        throw test_exception(exmsg_stream.str());
       }
-      first_it++;
-      second_it++;
     }
 
-    if (eq_flag) {
-      std::stringstream exmsg_stream;
-      exmsg_stream << "<test::container_not_equal failed!> " << message;
-      throw test_exception(exmsg_stream.str());
+    template <typename T1, typename T2>
+    static void gt(const T1& first, const T2& second,
+                   const std::string& message = std::string{}) {
+      if (first <= second) {
+        std::stringstream exmsg_stream;
+        exmsg_stream << "<test::gt failed!> " << message << " [first: " << first
+                     << "] [second: " << second << "]";
+        throw test_exception(exmsg_stream.str());
+      }
     }
-  }
-};
+
+    template <typename T1, typename T2>
+    static void lt(const T1& first, const T2& second,
+                   const std::string& message = std::string{}) {
+      if (first >= second) {
+        std::stringstream exmsg_stream;
+        exmsg_stream << "<test::lt failed!> " << message << " [first: " << first
+                     << "] [second: " << second << "]";
+        throw test_exception(exmsg_stream.str());
+      }
+    }
+
+    template <typename T1, typename T2>
+    static void geq(const T1& first, const T2& second,
+                    const std::string& message = std::string{}) {
+      if (first < second) {
+        std::stringstream exmsg_stream;
+        exmsg_stream << "<test::geq failed!> " << message << " [first: " << first
+                     << "] [second: " << second << "]";
+        throw test_exception(exmsg_stream.str());
+      }
+    }
+
+    template <typename T1, typename T2>
+    static void leq(const T1& first, const T2& second,
+                    const std::string& message = std::string{}) {
+      if (first > second) {
+        std::stringstream exmsg_stream;
+        exmsg_stream << "<test::leq failed!> " << message << " [first: " << first
+                     << "] [second: " << second << "]";
+        throw test_exception(exmsg_stream.str());
+      }
+    }
+
+    template <typename T1, typename T2>
+    static void container_equal(const T1& first, const T2& second,
+                                const std::string& message = std::string{}) {
+      if (first.size() != second.size()) {
+        std::stringstream exmsg_stream;
+        exmsg_stream << "<test::container_equal failed: "
+                     << (first.size() > second.size() ? "first" : "second")
+                     << " container is bigger> " << message;
+        throw test_exception(exmsg_stream.str());
+      }
+      auto first_it = first.cbegin();
+      auto second_it = second.cbegin();
+      std::size_t i = 0;
+      // Since containers have same lengths...
+      while (first_it != first.cend()) {
+        if (*first_it != *second_it) {
+          std::stringstream exmsg_stream;
+          exmsg_stream << "<test::container_equal failed: " << *first_it
+                       << " != " << *second_it << " at index #" << i << "> "
+                       << message;
+          throw test_exception(exmsg_stream.str());
+        }
+        first_it++;
+        second_it++;
+        i++;
+      }
+    }
+
+    template <typename T1, typename T2>
+    static void container_not_equal(const T1& first, const T2& second,
+                                    const std::string& message = std::string{}) {
+      if (first.size() != second.size()) return;
+
+      auto first_it = first.cbegin();
+      auto second_it = second.cbegin();
+      bool eq_flag = true;
+      // Since containers have same lengths...
+      while (first_it != first.cend()) {
+        if (*first_it != *second_it) {
+          eq_flag = false;
+          break;
+        }
+        first_it++;
+        second_it++;
+      }
+
+      if (eq_flag) {
+        std::stringstream exmsg_stream;
+        exmsg_stream << "<test::container_not_equal failed!> " << message;
+        throw test_exception(exmsg_stream.str());
+      }
+    }
+  };
 }
 #endif
